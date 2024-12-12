08a3901ae2c27c4bb48ce8c721ce5a983eb9c49c
ngochieu276 <ngochieu2761998@gmail.com>
2024-12-12 14:26:40 +0700

    tasks

---
+++ src/features/tasks/components/create-task-modal-wrapper.tsx
@@ -0,0 +1,57 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { useGetMembers } from "@/features/members/api/use-get-member";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { Loader } from "lucide-react";
+import { CreateTaskForm } from "./create-task-form";
+
+interface CreateTaskFormWrapperProps {
+  onCancel: () => void;
+}
+
+export const CreateTaskFormWrapper = ({
+  onCancel,
+}: CreateTaskFormWrapperProps) => {
+  const workspaceId = useWorkspaceId();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents?.map((project) => {
+    return {
+      id: project.$id,
+      name: project.name,
+      imageUrl: project.imageUrl,
+    };
+  });
+
+  const memberOptions = members?.documents?.map((member) => {
+    return {
+      id: member.$id,
+      name: member.name,
+    };
+  });
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  if (isLoading) {
+    return (
+      <Card className="w-full h-[714px] border-none shadow-none">
+        <CardContent className="flex items-center justify-center">
+          <Loader className=" size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <CreateTaskForm
+      onCancel={onCancel}
+      projectOptions={projectOptions ?? []}
+      memberOptions={memberOptions ?? []}
+    />
+  );
+};
---
+++ src/features/tasks/components/create-task-modal.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import ResponsiveModal from "@/components/responsive-modal";
+import useCreateTaskModal from "../hooks/use-create-task-modal";
+import { CreateTaskFormWrapper } from "./create-task-modal-wrapper";
+
+export const CreateTaskModal = () => {
+  const { isOpen, setIsOpen, close } = useCreateTaskModal();
+
+  return (
+    <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
+      <CreateTaskFormWrapper onCancel={close} />
+    </ResponsiveModal>
+  );
+};
