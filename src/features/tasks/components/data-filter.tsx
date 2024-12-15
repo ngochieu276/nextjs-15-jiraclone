5b8e87a4c916bda4a250896a6cd1e7c90db27c8f
ngochieu276 <ngochieu2761998@gmail.com>
2024-12-15 21:58:43 +0700

    data table

--- src/features/tasks/components/data-filter.tsx
+++ src/features/tasks/components/data-filter.tsx
@@ -19,6 +19,7 @@ interface DataFilterProps {
   hideProjectFilter?: boolean;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const DataFilter = ({ hideProjectFilter }: DataFilterProps) => {
   const workspaceId = useWorkspaceId();
 
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,8 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useQueryState } from "nuqs";
 import DataFilter from "./data-filter";
 import { useTaskFilter } from "../hooks/use-task-filters";
+import { DataTable } from "./data-table";
+import { columns } from "./column";
 
 const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilter();
@@ -61,7 +63,7 @@ const TaskViewSwitcher = () => {
         ) : (
           <>
             <TabsContent value="table" className="mt-0">
-              {JSON.stringify(tasks)}
+              <DataTable columns={columns} data={tasks?.documents ?? []} />
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
               {JSON.stringify(tasks)}
