532cf61d721e37a2bdcfadce6532eec57122bbff
ngochieu276 <ngochieu2761998@gmail.com>
2024-12-28 22:15:57 +0700

    fix join wp client page

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,12 +1,12 @@
 import { getCurrent } from "@/features/auth/queries";
 import { redirect } from "next/navigation";
-import WorkspaceIdSettingClientPage from "../../settings/client";
+import WorkspaceIdJoinClientPage from "./client";
 
 const WorkspaceIdJoinPage = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return <WorkspaceIdSettingClientPage />;
+  return <WorkspaceIdJoinClientPage />;
 };
 
 export default WorkspaceIdJoinPage;
